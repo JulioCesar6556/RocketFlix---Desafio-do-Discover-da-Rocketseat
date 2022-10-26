a818674065b2cd422c74a22ac4519d7c632bc396
Julio <juliooliveiracesar06@gmail.com>
2022-10-26 10:59:37 -0300

    Criação do backend

---
+++ api.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'api_key=88e0ee57775341e33bc2a000e53fb4bf';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
--- main.js
+++ main.js
@@ -3,3 +3,32 @@ import {
   IMG_URL,
   language,
 } from './api.js'
+
+  const mudarFilme = document.querySelector("#mudar-filme");
+
+  function chamarFilme (){
+    const id = Math.floor(Math.random() *1000) + 1
+    const url = `${BASE_URL}${id}?${API_KEY}&${language}`
+    axios.get(url).then(response => {
+      const data = response.data
+      filme.innerHTML =
+        <><div>
+          <img class='poster' src="${IMG_URL + data.poster_path}" alt="Poster do Fime"></img>
+          <p class="popularidade"> Nota: ${data.vote_average}</p>
+        </div>
+        <div>
+            <h2 class="titulo-do-filme"></h2>
+            <p class="Descrição-do-filme">${data.overview}</p>
+          </div></>
+    })
+    .catch(error => {
+      filme.innerHTML = 
+      <div id="error-mensage">
+        <h2 class="titulo-do-filme">Hoje não é dia de filme. Keep calm and play blass.</h2>
+      </div>
+    })
+  }
+  mudarFilme.addEventListener("click", chamarFilme);
+
+
+
